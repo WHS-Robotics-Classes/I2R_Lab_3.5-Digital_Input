869c6c1ce13e8273b754e1543f5bc69612e324ad
Steve Cline <steve.cline@weldre4.org>
2021-02-07 16:23:41 -0700

    Update Starter.ino

--- Starter.ino
+++ Starter.ino
@@ -1,11 +1,13 @@
+int inPin = 7;    // pushbutton connected to digital pin 7
+int val = 0;      // variable to store the read value
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println(); // put your message that you would like to display in the serial monitor in quotes inside the parenthesis here
+  Serial.begin ();
+  pinMode(inPin, );    // sets the digital pin 7 as input
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  val = digitalRead(inPin);   // read the input pin
+  // print val as a binary number to the Serial Monitor using Serial.println()
+  // delay 500 ms
 }
-
